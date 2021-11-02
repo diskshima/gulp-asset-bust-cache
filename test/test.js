4df4d7cc018f78b692bf7eed51f334b021c8b96a
Daisuke Shimamoto <diskshima@gmail.com>
2021-11-03 00:14:50 +0900

    Add test case

--- test/test.js
+++ test/test.js
@@ -40,4 +40,41 @@ describe('gulp-bust-cache', function () {
       stream.end();
     });
   });
+
+  describe('duplicate elements ', function () {
+    const expectedFile = new File({
+      cwd: 'test/',
+      base: 'test/data/',
+      path: 'test/data/duplicate_element_expected.html',
+      contents: fs.readFileSync('test/data/duplicate_element_expected.html'),
+    });
+
+    it('should both be cache busted', function (done) {
+      const beforeFile = new File({
+        cwd: 'test/',
+        base: 'test/data/',
+        path: 'test/data/duplicate_element_before.html',
+        contents: fs.readFileSync('test/data/duplicate_element_before.html'),
+      });
+
+      let newFileContent = null;
+
+      const stream = bustCache();
+
+      stream.on('data', function (newFile) {
+        newFileContent = newFile.contents;
+      });
+      stream.on('end', function () {
+        String(newFileContent).should.equal(String(expectedFile.contents));
+        done();
+      });
+      stream.on('error', function (err) {
+        should.exist(err);
+        done(err);
+      });
+
+      stream.write(beforeFile);
+      stream.end();
+    });
+  });
 });
